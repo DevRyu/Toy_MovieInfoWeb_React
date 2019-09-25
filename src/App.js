602b2f43898e1617637ce952f40b2ab86b8980b3
Dev Ryu <eostmghf01@gmail.com>
2019-09-25 18:23:46 +0900

    2-1 console

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Potato from "./Potato";
 
+function Food(props) {
+  console.log(props);
+  return <h1>I Like Potato</h1>;
+}
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food name="kimchi" something={true} papa={["hello", 1, 2, 3]} />
     </div>
   );
 }
