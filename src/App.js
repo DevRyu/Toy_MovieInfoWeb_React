58defaf5787d6edbca5ad31f46e12cb72c1b571e
Dev Ryu <eostmghf01@gmail.com>
2019-09-27 23:59:05 +0900

    2-2(1)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React from "react";
 function Food({ name }) {
   return <h1>I Like {name}</h1>;
 }
+const foodILike = [];
+// 음식 배열을 통해넣음
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Food name="kimchi" />
-      <Food name="gogi" />
-      <Food name="ramen" />
-      <Food name="soup" />
     </div>
   );
 }
