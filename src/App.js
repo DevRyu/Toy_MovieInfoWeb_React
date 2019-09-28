d7bd57362ea5fc33ee6a01301f089aa166855ad0
Dev Ryu <eostmghf01@gmail.com>
2019-09-28 23:41:36 +0900

    0928

--- src/App.js
+++ src/App.js
@@ -14,3 +14,4 @@ function App() {
 }
 
 export default App;
+//app 화면
--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // 리엑트 돔 렌더로 APP.js에서 아이디를 가져와서 변경시켜준다.
+//인덱스 화면
