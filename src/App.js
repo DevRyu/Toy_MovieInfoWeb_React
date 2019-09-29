08e63f6b51f753a71a83c913a50d65df87fa5907
Dev Ryu <eostmghf01@gmail.com>
2019-09-29 15:01:34 +0900

    2-2-

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import React from "react";
 function Food({ name }) {
   return <h1>I Like {name}</h1>;
 }
-const foodILike = [];
+const foodILike = [
+  {
+    name: "A"
+  },
+  {
+    name: "B"
+  },
+  {
+    name: "C"
+  }
+];
 // 음식 배열을 통해넣음
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <div>im </div>
+      {foodILike.map(dish => (
+        <Food name={dish.name} />
+      ))}
     </div>
   );
+  //디쉬는 오브젝트
 }
 
 export default App;
